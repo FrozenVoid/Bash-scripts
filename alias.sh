9312be8f8f68f15df05e7b5a1cb7d9af557a1ff4
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-10 18:35:37 +0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,3 +1,27 @@
 alias dir='ls -ABgtr  --color=always --group-directories-first --dereference-command-line -S'
 alias net='sudo watch ss -tonp'
 alias netstat='ss'
+alias disktree='ncdu'
+alias edit='featherpad'
+alias diff="sync&&git diff --patch --patience --diff-filter=M --ignore-all-space  --no-index --word-diff=color  --color=always -b "
+alias stack="sudo perf top -g"
+alias vars='printenv'
+alias cls='reset'
+
+alias up='sudo apt-get install --only-upgrade '
+alias strings='strings --all'
+alias rem='sudo apt purge'
+alias ins='sudo apt-get install'
+alias asm='objdump -SCl -Mintel'
+alias ed='mousepad'
+alias lock='echo "screen lock ON:to disable type pass+enter";xtrlock'
+alias io='sudo iotop -a'
+alias ed='mousepad'
+alias shut='sudo shutdown -h now'
+alias reb='sudo shutdown -r now'
+alias where='locate'
+function bashrc(){
+ed ~/.bashrc  
+wait
+source ~/.bashrc
+}
