4d6fc1a7724dee47d529533cd1b96e457713b7bf
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-12-01 12:53:53 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,9 +1,11 @@
+#cloneuser github_name
 function cloneuser(){
 curl -s https://api.github.com/users/$1/repos?per_page=200 | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone --recursive
 }
 
+#uprepos //current directory scan
 function uprepos(){
-find . -name .git -type d | rev | cut -c 6- | rev | xargs -I {} git -C {} pull
+find . -name .git -type d | rev | cut -c 6- | rev | xargs -I {} git -C {} pull --recurse-submodules
 }
 
 
