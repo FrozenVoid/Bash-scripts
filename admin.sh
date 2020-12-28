dca2692aac20759e85b573e4f53b84e96df9bd95
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-12-28 20:06:05 +0200

    Create admin.sh

---
+++ admin.sh
@@ -0,0 +1,10 @@
+#renice process_name
+function pnice(){
+sudo renice $1 $(pgrep $2)
+}
+#get top cpu processes sorted by use
+alias pstop='ps -auxk pcpu'
+#kill top process
+function pktop(){
+pkill $(ps -auxk pcpu|tail -n 1|grep -oE '[^ ]+$')
+}
