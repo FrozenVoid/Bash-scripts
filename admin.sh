25f7c8e0e6d82e53ee7d1cad268046c5a76ab7fa
FrozenVoid <FrozenVoid@users.noreply.github.com>
2021-01-04 18:43:23 +0200

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -3,7 +3,7 @@ function pnice(){
 sudo renice $1 $(pgrep $2)
 }
 #get top cpu processes sorted by use
-alias pstop='ps -auxk pcpu|less'
+alias pstop='ps -auxk -pcpu|less'
 #kill top process
 function pktop(){
 pkill $(ps -auxk pcpu|tail -n 1|grep -oE '[^ ]+$')
